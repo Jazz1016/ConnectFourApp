095c12c72d42914c99ad6e56a0b477ad51b429c7
Jantsen Tanner <jantsentanner@gmail.com>
2021-05-19 13:28:01 -0600

    added outlets and actions

---
+++ TicTacToe/TicTacToe/Controllers/ViewControllers/TicTacToe1ViewController.swift
@@ -0,0 +1,58 @@
+//
+//  TicTacToe1ViewController.swift
+//  TicTacToe
+//
+//  Created by Jantsen Tanner on 5/19/21.
+//
+
+import UIKit
+
+class TicTacToe1ViewController: UIViewController {
+
+    @IBOutlet weak var a1button: UIButton!
+    @IBOutlet weak var a2button: UIButton!
+    @IBOutlet weak var a3button: UIButton!
+    @IBOutlet weak var b1button: UIButton!
+    @IBOutlet weak var b2button: UIButton!
+    @IBOutlet weak var b3button: UIButton!
+    @IBOutlet weak var c1button: UIButton!
+    @IBOutlet weak var c2button: UIButton!
+    @IBOutlet weak var c3button: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func a1button(_ sender: Any){
+        
+    }
+    @IBAction func a2button(_ sender: Any){
+        
+    }
+    @IBAction func a3button(_ sender: Any){
+        
+    }
+    @IBAction func b1button(_ sender: Any){
+        
+    }
+    @IBAction func b2button(_ sender: Any){
+        
+    }
+    @IBAction func b3button(_ sender: Any){
+        
+    }
+    @IBAction func c1button(_ sender: Any){
+        
+    }
+    @IBAction func c2button(_ sender: Any){
+        
+    }
+    @IBAction func c3button(_ sender: Any){
+        
+    }
+    
+
+
+}
