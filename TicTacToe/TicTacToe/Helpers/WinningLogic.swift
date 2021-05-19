798bf1bd7c43a0abfae9f71f37a4b7bc8e8adb46
James Lea <jameslea@Jamess-MBP.localdomain>
2021-05-19 12:32:19 -0600

    started on winning logic

---
+++ TicTacToe/TicTacToe/Helpers/WinningLogic.swift
@@ -0,0 +1,32 @@
+//
+//  WinningLogic.swift
+//  TicTacToe
+//
+//  Created by James Lea on 5/19/21.
+//
+
+import Foundation
+
+class WinningLogic {
+    
+    let a1 = ""
+    let b1 = ""
+    let c1 = ""
+    let a2 = ""
+    let b2 = ""
+    let c2 = ""
+    let a3 = ""
+    let b3 = ""
+    let c3 = ""
+    
+    
+    func WinningLogic() {
+        if a1 == "x" && a2 == "x" && a3 == "x" {
+            return "X wins"
+        }
+        
+        if
+        
+    }
+    
+}
